0bcf916426323764754c82eb805503dff51abfef
online5880 <gjtjqkr1@naver.com>
2021-06-13 03:43:58 +0900

    #04

--- Source/June/Private/Item_Base.cpp
+++ Source/June/Private/Item_Base.cpp
@@ -12,7 +12,7 @@ AItem_Base::AItem_Base()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	SetReplicates(true);
+	//SetReplicates(true);
 
 	Body_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body_Mesh"));
 	RootComponent = Body_Mesh;
--- Source/June/Private/Projectile_Base.cpp
+++ Source/June/Private/Projectile_Base.cpp
@@ -11,7 +11,7 @@ AProjectile_Base::AProjectile_Base()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 
-	SetReplicates(true);
+	//SetReplicates(true);
 
 	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Component"));
 	CapsuleComponent->SetCollisionObjectType(ECC_PhysicsBody);
